afeb5c7a7024174fc33da485fbe01bf41d0dc411
Julio Marques <juliomarquesjr@yahoo.com.br>
2022-08-02 15:05:29 -0300

    Classe envio de emails - Rev 2

--- enviaEmail.py
+++ enviaEmail.py
@@ -1,30 +1,27 @@
 import smtplib as smt
 
 class Email:
-    def __init__(self, destinatario, assunto, host, usuario, senha, porta):
+    def __init__(self, destinatario, assunto):
         self._destinatario = destinatario
         self._assunto = assunto
-        self._host = host
-        self._usuario = usuario
-        self._senha = senha
-        self._porta = porta
-
-    def _conectar(self):
-        server = smt.SMTP_SSL(self._host, self._porta)
-        server.login(self._usuario, self._senha)
-        return server
+        self._host = 'mail.maximweb.com.br' #Buscar info do banco de dados
+        self._usuario = 'julio@maximweb.com.br' #Buscar info do banco de dados
+        self._senha = 'bTv6Y7QHw1ZT' #Buscar info do banco de dados
+        self._porta = 465 #Buscar do banco de dados
 
     def enviar_mensagem(self, mensagem):
-        server = self._conectar()
-
-        if(server):
+        try:
+            server = smt.SMTP_SSL(self._host, self._porta)
+            server.login(self._usuario, self._senha)
             self.mensagem = mensagem
             server.sendmail(self._usuario, self._destinatario, f'Subject: {self._assunto} \n{self.mensagem}')
-            server.quit()
             return True
-        else:
+        except:
+            print("Erro ao enviar email")
             return False
+        finally:
+            server.quit()
 
 if __name__ == "__main__":
-    email = Email('juliomarquesjr@yahoo.com.br', 'Teste Classe Email', 'mail.maximweb.com.br','julio@maximweb.com.br', 'bTv6Y7QHw1ZT', '465')
-    print("Mensagem Envida") if email.enviar_mensagem('Teste de email da classe Email') else print("Deu erro")
+    email = Email('juliomarquesjr@yahoo.com.br', 'Teste Classe Email')
+    print(email.enviar_mensagem('Teste de email da classe Email'))
