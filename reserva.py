a08372dcfedd2b4c43998150227cff3453696a1a
juubi <juubiter@gmail.com>
2022-08-02 20:22:45 -0300

    commit inicial classe Reserva

---
+++ reserva.py
@@ -0,0 +1,18 @@
+class Reserva:
+    def __init__(self, id_ferramenta, cpf, descricao, data_retirada, data_devolucao):
+        self.id_ferramenta = id_ferramentaself.cpf = cpf
+        self.descricao = descricao
+        self.data_retirada = data_retirada
+        self.data_devolucao = data_devolucao
+
+    def reserva(self):
+        pass
+
+    def envia_email(self):
+        pass
+
+    def consulta_reserva(self):
+        pass
+
+    def impressao_reservas(self):
+        pass
