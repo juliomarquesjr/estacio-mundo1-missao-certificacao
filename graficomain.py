445bed83163e7067429acb62c7cd896cc32d2d60
Julio Marques <juliomarquesjr@yahoo.com.br>
2022-08-04 15:28:39 -0300

    Adicionado a lista de reservas

--- graficomain.py
+++ graficomain.py
@@ -1,5 +1,6 @@
 import tkinter
 from tkinter import Label, PhotoImage, Button
+from tkinter.ttk import Treeview
 import tkinter as tk
 import tkinter.font as tkFont
 
@@ -23,14 +24,27 @@ class GraficoMain:
         self.lbsistema = Label(self.principal, text="Sistema de Reservas",font=tkFont.Font(size=20))
         self.lbexpira = Label(self.principal, text="Reservas Expirando",font=tkFont.Font(size=15))
 
+        ## Inicio da Lista de Reservas
         self.bttecnico = Button(self.principal, image=self.icon_tecnico, height=22, compound='left', padx=5, text="Técnicos", command=GraficoConsultaTecnico)
         self.btferramenta = Button(self.principal, image=self.icon_ferramenta, height=22, compound='left', padx=5, text="Ferramentas")
         self.btreservar = Button(self.principal, image=self.icon_reserva, height=22, compound='left', padx=5, text="Reservas")
         self.btconfig = Button(self.principal, text="Configurações", image=self.icon_config, height=22, padx=5, compound='left', command=GraficoConfig)
         self.btsair = Button(self.principal, text="Sair", image=self.icon_saida, compound='left', height=22, padx=5, command=self.principal.destroy)
 
+        self.nomes_colunas = ('col1', 'col2', 'col3')
+        self.lista_reservas = Treeview(self.principal, columns=self.nomes_colunas, show='headings', height=12)
+        self.lista_reservas.column('col1', width=205, stretch=False)
+        self.lista_reservas.column('col2', width=205, stretch=False)
+        self.lista_reservas.column('col3', width=165, stretch=False)
+
+        self.lista_reservas.heading('col1', text='Ferramenta')
+        self.lista_reservas.heading('col2', text='Tecnico')
+        self.lista_reservas.heading('col3', text="Previsão de Entrega")
+        ## Fim da lista de reserva
+
         self.lbsistema.pack(side=tk.TOP)
         self.lbexpira.place(x=10,y=50)
+        self.lista_reservas.place(x=10, y=80)
         self.bttecnico.place(x=10,y=360)
         self.btferramenta.place(x=100,y=360)
         self.btreservar.place(x=210,y=360)
