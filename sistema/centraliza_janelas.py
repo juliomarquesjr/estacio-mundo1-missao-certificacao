7482a2847075991ce3895ecb8b505615638e0a1a
Julio Marques <juliomarquesjr@yahoo.com.br>
2022-08-04 11:48:44 -0300

    Função para centralizar as janelas

---
+++ sistema/centraliza_janelas.py
@@ -0,0 +1,27 @@
+def center(win):
+    # :param win: the main window or Toplevel window to center
+
+    # Apparently a common hack to get the window size. Temporarily hide the
+    # window to avoid update_idletasks() drawing the window in the wrong
+    # position.
+    win.update_idletasks()  # Update "requested size" from geometry manager
+
+    # define window dimensions width and height
+    width = win.winfo_width()
+    frm_width = win.winfo_rootx() - win.winfo_x()
+    win_width = width + 2 * frm_width
+
+    height = win.winfo_height()
+    titlebar_height = win.winfo_rooty() - win.winfo_y()
+    win_height = height + titlebar_height + frm_width
+
+    # Get the window position from the top dynamically as well as position from left or right as follows
+    x = win.winfo_screenwidth() // 2 - win_width // 2
+    y = win.winfo_screenheight() // 2 - win_height // 2
+
+    # this is the line that will center your window
+    win.geometry('{}x{}+{}+{}'.format(width, height, x, y))
+
+    # This seems to draw the window frame immediately, so only call deiconify()
+    # after setting correct window position
+    win.deiconify()
