3612f4fac384a9870fb12bfecf57a21d5443cfa5
valeriasms <valeriasouza.projetos@gmail.com>
2022-08-05 03:16:14 -0300

    test commit com call me

--- graficoferramenta.py
+++ graficoferramenta.py
@@ -1,68 +1,81 @@
 import tkinter
-from tkinter import Label
-from tkinter import Entry
-from tkinter import Button
 
-def acao():
-    print("Pressionado")
+from tkinter import Label, Entry, Button, messagebox, PhotoImage
 
-principal = tkinter.Tk()
-principal.geometry("500x230")
-principal.title('Ferramentas')
+from ferramenta import Ferramenta
+from sistema.centraliza_janelas import center
 
 
-lbcodigo = Label(principal, text="Código: ")
-lbtamanho = Label(principal, text="Tamanho: ")
-lbvolts = Label(principal, text="Voltagem: ")
-lbund = Label(principal, text="Unidade: ")
-lbtipo = Label(principal, text="Tipo: ")
-lbmat = Label(principal, text="Material: ")
-lbref = Label(principal, text="Referência: ")
-lbtemp = Label(principal, text="Tempo Limite: ")
-lbfab = Label(principal, text="Fabricante: ")
-lbdesc = Label(principal, text="Descrição: ")
+class GraficoFerramenta(Ferramenta):
+    def __init__(self):
+        #super().__init__()
+        self._principal = tkinter.Toplevel() #Top Level pois ela é filha de graficomain.py
+        self._principal.geometry("500x230")
+        self._principal.minsize(500, 230)
+        self._principal.maxsize(500, 230)
+        self._principal.title('Ferramentas')
+        center(self._principal)
 
+        #self.icon_editar = PhotoImage(file="assets/icones/icon_editar.png")
+        self.icon_salvar = PhotoImage(file="assets/icones/icon_salvar.png")
+        self.icon_sair = PhotoImage(file="assets/icones/icon_saida.png")
 
-cxcodigo = Entry(principal, width = 15)
-cxtamanho = Entry(principal, width = 25)
-cxvolts = Entry(principal, width = 20)
-cxund = Entry(principal, width = 20)
-cxtipo = Entry(principal, width = 20)
-cxmat = Entry(principal, width = 20)
-cxref = Entry(principal, width = 30)
-cxtemp = Entry(principal, width = 7)
-cxfab = Entry(principal, width = 35)
-cxdesc = Entry(principal, width = 65)
+        self.lbcodigo = Label(self._principal, text="Código: ")
+        self.lbtamanho = Label(self._principal, text="Tamanho: ")
+        self.lbvolts = Label(self._principal, text="Voltagem: ")
+        self.lbund = Label(self._principal, text="Unidade: ")
+        self.lbtipo = Label(self._principal, text="Tipo: ")
+        self.lbmat = Label(self._principal, text="Material: ")
+        self.lbref = Label(self._principal, text="Referência: ")
+        self.lbtemp = Label(self._principal, text="Tempo Limite: ")
+        self.lbfab = Label(self._principal, text="Fabricante: ")
+        self.lbdesc = Label(self._principal, text="Descrição: ")
 
+        self.cxcodigo = Entry(self._principal, width=15)
+        self.cxtamanho = Entry(self._principal, width=25)
+        self.cxvolts = Entry(self._principal, width=20)
+        self.cxund = Entry(self._principal, width=20)
+        self.cxtipo = Entry(self._principal, width=20)
+        self.cxmat = Entry(self._principal, width=20)
+        self.cxref = Entry(self._principal, width=30)
+        self.cxtemp = Entry(self._principal, width=7)
+        self.cxfab = Entry(self._principal, width=35)
+        self.cxdesc = Entry(self._principal, width=65)
 
-bteditar = Button(principal, text="Editar", command=acao)
-btsalvar = Button(principal, text="Salvar", command=acao)
-btfechar = Button(principal, text="Fechar", command=principal.quit)
+#bteditar = Button(principal, text="Editar", command=acao)
+#btsalvar = Button(principal, text="Salvar", command=acao)
+#btfechar = Button(principal, text="Fechar", command=principal.quit)
 
+        self.bteditar = Button(self._principal, image=self.icon_editar, compound='left', height=22, padx=5,
+                               text="Editar", command=self._editar)
+        self.btsalvar = Button(self._principal, image=self.icon_salvar, compound='left', height=22, padx=5,
+                               text="Salvar", command=self._salvar)
+        self.btfechar = Button(self._principal, image=self.icon_sair, compound='left', height=22, padx=5,
+                               text="Fechar", command=self._principal.destroy)
 
-lbcodigo.place(x=10,y=10)
-cxcodigo.place(x=60,y=10)
-lbtamanho.place(x=260,y=10)
-cxtamanho.place(x=320,y=10)
-lbvolts.place(x=10,y=40)
-cxvolts.place(x=70,y=40)
-lbund.place(x=295,y=40)
-cxund.place(x=350,y=40)
-lbtipo.place(x=10,y=70)
-cxtipo.place(x=45,y=70)
-lbmat.place(x=295,y=70)
-cxmat.place(x=350,y=70)
-lbref.place(x=10,y=100)
-cxref.place(x=75,y=100)
-lbtemp.place(x=340,y=100)
-cxtemp.place(x=425,y=100)
-lbfab.place(x=10,y=130)
-cxfab.place(x=75,y=130)
-lbdesc.place(x=10,y=160)
-cxdesc.place(x=75,y=160)
-bteditar.place(x=10,y=195)
-btsalvar.place(x=390,y=195)
-btfechar.place(x=440,y=195)
+        self.lbcodigo.place(x=10, y=10)
+        self.cxcodigo.place(x=60, y=10)
+        self.lbtamanho.place(x=260, y=10)
+        self.cxtamanho.place(x=320, y=10)
+        self.lbvolts.place(x=10, y=40)
+        self.cxvolts.place(x=70, y=40)
+        self.lbund.place(x=295, y=40)
+        self.cxund.place(x=350, y=40)
+        self.lbtipo.place(x=10, y=70)
+        self.cxtipo.place(x=45, y=70)
+        self.lbmat.place(x=295, y=70)
+        self.cxmat.place(x=350, y=70)
+        self.lbref.place(x=10, y=100)
+        self.cxref.place(x=75, y=100)
+        self.lbtemp.place(x=340, y=100)
+        self.cxtemp.place(x=425, y=100)
+        self.lbfab.place(x=10, y=130)
+        self.cxfab.place(x=75, y=130)
+        self.lbdesc.place(x=10, y=160)
+        self.cxdesc.place(x=75, y=160)
+        self.bteditar.place(x=10, y=195)
+        self.btsalvar.place(x=390, y=195)
+        self.btfechar.place(x=440, y=195)
 
-
-principal.mainloop()
+        if __name__ == "__main__":
+            GraficoFerramenta()
